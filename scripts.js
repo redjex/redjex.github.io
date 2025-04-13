7af5c142e0a29ab14752d7f84417d19b9ebccf4f
redjex <redjexs@gmail.com>
2025-04-13 21:34:29 +0300

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function openTab(tabId) {
+    document.querySelectorAll('.tab-content').forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.querySelectorAll('.tab-button').forEach(button => {
+        button.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+    event.target.classList.add('active');
+}
