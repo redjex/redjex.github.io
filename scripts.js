cdbdc8b3b6eb17d166c2056410f472ddbd1f9af2
redjex <redjexs@gmail.com>
2025-05-20 23:02:55 +0500

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -26,7 +26,7 @@ document.addEventListener('DOMContentLoaded', () => {
     };
 
     // Отслеживаем клики и завершенные квадраты
-    let crystalClicks = 1;
+    let crystalClicks = 0;
     let completedSquares = new Set();
 
     // Обновляем проценты для всех квадратов
